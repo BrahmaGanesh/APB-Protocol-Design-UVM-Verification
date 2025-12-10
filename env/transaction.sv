a46c9800a780204ef38ae8a44e2eef3e8e5db3b9
BrahmaGanesh <katrapallibrahmaganesh@gmail.com>
2025-12-10 16:51:02 +0530

    Add all constraints in transaction class to improve coverage and randomization control

--- env/transaction.sv
+++ env/transaction.sv
@@ -11,6 +11,26 @@ class transaction #(
          bit     pready;
          bit     pslverr;
 
+    constraint write  {pwrite inside {0,1};}
+    constraint ready  {pready inside {0,1};}
+    constraint slverr {pslverr inside {0,1};}
+    constraint addr {soft paddr inside {
+        [0:31],
+        [32:127],
+        [128:191],
+        [192:255]
+    };}
+    constraint wdata {if(pwrite){
+        pwdata inside {
+            32'h0000_0000,
+            32'hFFFF_FFFF,
+            [0:255],
+            [32'h0000_FF00 : 32'h0000_FFFF]
+        };
+    }else
+    pwdata == '0;
+    }
+
     function new(string name = "transaction");
         super.new(name);
     endfunction
